360fa076c5cc550c0200646d2bbd16a49ed1451a
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2025-07-31 20:23:32 -0400

    Starting decoder

---
+++ src/app/components/MetarDecoder.tsx
@@ -0,0 +1,9 @@
+export default function MetarDecoder() {
+    return (
+        <div>
+        <h1>Metar Decoder</h1>
+        <p>This component will decode METAR reports.</p>
+        {/* Add your decoding logic here */}
+        </div>
+    );
+}
